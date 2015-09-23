5c326d8b027fe937f11b50ddf29920efca12696f
Denis Solonenko <denis.solonenko@gmail.com>
2015-09-23 20:09:12 +0800

    Fix prepare_dropbox_keys.sh

--- prepare_dropbox_keys.sh
+++ prepare_dropbox_keys.sh
@@ -2,4 +2,4 @@
 sed -i -b s/"db-INSERT-APP-KEY-HERE"/"db-$1"/ ./Financisto/src/main/AndroidManifest.xml
 sed -i -b s/"INSERT_APP_KEY_HERE"/"$1"/ ./Financisto/src/main/java/ru/orangesoftware/financisto2/export/dropbox/Dropbox.java
 sed -i -b s/"INSERT_APP_SECRET_HERE"/"$2"/ ./Financisto/src/main/java/ru/orangesoftware/financisto2/export/dropbox/Dropbox.java
-echo "Done, don't forget to call bzr revert after building apk"
+echo "Done, don't forget to revert the changes after building the release apk"
